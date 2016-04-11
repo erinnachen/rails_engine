b16d11ca625bca68868a5e93b893d69f296287a6
Erinna Chen <erinna.chen@gmail.com>
2016-04-11 16:03:12 -0600

    Add find merchant by specific field, case-insensitive

--- app/controllers/api/v1/merchants_controller.rb
+++ app/controllers/api/v1/merchants_controller.rb
@@ -3,4 +3,17 @@ class Api::V1::MerchantsController < Api::ApiController
   def index
     respond_with Merchant.all
   end
+
+  def show
+    respond_with Merchant.find(params[:id])
+  end
+
+  def find
+    if params[:id]
+      respond_with Merchant.find(params[:id].to_i)
+    elsif params[:name]
+      respond_with Merchant.where('lower(name) = ?', params[:name].downcase).take
+    end
+  end
+
 end
