b34c929ccfad6deea2d50a8aee4034eb4120b115
Erinna Chen <erinna.chen@gmail.com>
2016-06-01 23:36:14 -0600

    Fix most_item and most_revenue method on Item

--- app/models/item.rb
+++ app/models/item.rb
@@ -9,46 +9,29 @@ class Item < ActiveRecord::Base
   validates :merchant_id, presence: true
 
   def best_day
-    invoices.paid.select("invoices.created_at, sum(invoice_items.quantity) as items_sold").group("invoices.created_at").order("items_sold DESC").first
+    invoices.paid
+      .select("invoices.created_at, sum(invoice_items.quantity) as items_sold")
+      .group("invoices.created_at")
+      .order("items_sold DESC")
+      .first
   end
 
   def self.most_items(quantity)
     select("items.*, sum(quantity) as items_sold")
-        .joins(invoices: :transactions)
-        .where(transactions: {result: "success"})
-        .group("items.id")
-        .order("items_sold DESC")
-        .first(quantity)
+      .joins(invoices: :transactions)
+      .where(transactions: {result: "success"})
+      .group("items.id")
+      .order("items_sold DESC")
+      .first(quantity)
   end
 
   def self.most_revenue(quantity)
-    # select("items.*, SUM('quantity*invoice_items.unit_price') as revenue")
-    #     .joins(invoices: :transactions)
-    #     .where(transactions: {result: "success"})
-    #     .group("items.id")
-    #     .order("revenue DESC")
-    #     .first(quantity)
-
-    #all.sort_by {|item| -1*item.total_revenue }.first(quantity)
+    select("items.*, sum(quantity*invoice_items.unit_price) as revenue")
+      .joins(invoices: :transactions)
+      .where(transactions: {result: "success"})
+      .group("items.id")
+      .order("revenue DESC")
+      .first(quantity)
   end
-  #
-  # def total_revenue
-  #   invoice_items.reduce(0) do |sum, inv_item|
-  #     if inv_item.invoice.successful?
-  #       sum+inv_item.unit_price*inv_item.quantity
-  #     else
-  #       sum
-  #     end
-  #   end
-  # end
-  #
-  # def total_sold
-  #   invoice_items.reduce(0) do |sum, inv_item|
-  #     if inv_item.invoice.successful?
-  #       sum+inv_item.quantity
-  #     else
-  #       sum
-  #     end
-  #   end
-  # end
+
 end
